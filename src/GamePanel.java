387a923f41dddaa657dd7e52cea355e8092fb76c
Raumjojo <johannesschulzeherding@gmail.com>
2024-02-23 17:09:25 +0100

    TEst 1

---
+++ src/GamePanel.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class GamePanel extends JPanel {
+
+    // SCREEN SETTINGS
+    dinal int originalTileSize = 16;//16x16 tile
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! Amazon Correto");
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Red-Sands");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
     }
 }
