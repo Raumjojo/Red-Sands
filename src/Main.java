560feac1eab8b964b4bed5a77c647ea59a360fd3
Raumjojo <johannesschulzeherding@gmail.com>
2024-02-23 17:16:48 +0100

    TEst 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Amazon Correto");
+        System.out.println("Hello world! Amazon Correto ist Amaton");
 
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
