0308b38186c3a4f1a09a85141136af920b3568d2
Raumjojo <johannesschulzeherding@gmail.com>
2024-02-24 12:50:42 +0100

    .3 TileMap video #4 end

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -4,16 +4,22 @@ import main.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Objects;
 
 public class TileManager {
     GamePanel gamePanel;
     Tile[] tiles;
+    int mapTileNumber[][];
     public TileManager(GamePanel gamePanel){
         this.gamePanel = gamePanel;
         tiles = new Tile[10];
+        mapTileNumber = new int[gamePanel.getMaxScreenCol()][gamePanel.getMaxScreenCol()];
         getTileImage();
+        loadMap();
     }
     public void getTileImage(){
         try{
@@ -21,15 +27,43 @@ public class TileManager {
             tiles[0].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/redSand01.png")));
             tiles[1] = new Tile();
             tiles[1].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/redSand02.png")));
+
+            //additionally water, more or less
+            tiles[2] = new Tile();
+            tiles[2].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/waterWaves01.png")));
         }catch (IOException e){
             e.printStackTrace();
         }
     }
+    public void loadMap(){
+
+        try {
+            InputStream inputStream = getClass().getResourceAsStream("/maps/map01.txt");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            for (int iRow=0; iRow < gamePanel.getMaxScreenRow(); iRow++){
+                String line = bufferedReader.readLine();
+                for (int iCol=0; iCol < gamePanel.getMaxScreenCol(); iCol++) {
+                    String numbers[] = line.split(" ");
+
+                    int num = Integer.parseInt(numbers[iCol]);
+
+                    mapTileNumber[iCol][iRow] = num;
+                }
+            }
+            bufferedReader.close();
+
+        }catch(Exception e){
+
+        }
+
+    }
     public void draw(Graphics2D g2){
         int tileSize = gamePanel.getTileSize();
-        for (int i = 0; i < gamePanel.getMaxScreenCol(); i++){
-            for (int j = 0; j < gamePanel.getMaxScreenRow(); j++){
-                g2.drawImage(tiles[1].image,i*tileSize,j*tileSize,tileSize,tileSize,null);
+        for (int iCol = 0; iCol < gamePanel.getMaxScreenCol(); iCol++){
+            for (int iRow = 0; iRow < gamePanel.getMaxScreenRow(); iRow++){
+                int tileNum = mapTileNumber[iCol][iRow];
+                g2.drawImage(tiles[tileNum].image,iCol*tileSize,iRow*tileSize,tileSize,tileSize,null);
             }
         }
 
